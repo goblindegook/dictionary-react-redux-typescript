9cdadef269b1975d60665f36d075a2ccb1577f3c
Luis Rodrigues <lrodrigues@equalexperts.com>
2016-07-19 11:26:54 +0100

    Refactor entry factory specs

--- test/api/Entry.spec.ts
+++ test/api/Entry.spec.ts
@@ -1,25 +1,49 @@
 import * as expect from "expect";
 import { createEntry } from "../../src/api/Entry";
 
-describe("Entry creator", () => {
-  describe("New entry from word", () => {
+/* tslint:disable:no-var-requires */
+const defineFixture = require("./fixtures/define-a.json");
+/* tslint:enable:no-var-requires */
+
+describe("Entry factory", () => {
+  context("when creating an entry from word", () => {
     const word = "word";
     const entry = createEntry(word);
 
-    it("has an ID equal to word", () => {
+    it("sets an ID equal to word", () => {
       expect(entry.id).toBe(word);
     });
 
-    it("has a word", () => {
+    it("sets the word", () => {
       expect(entry.word).toBe(word);
     });
 
-    it("has no sense", () => {
+    it("sets no senses", () => {
       expect(entry.senses).toEqual([]);
     });
   });
 
-  describe("New entry from complete data", () => {
+  context("when creating an entry from null data", () => {
+    let id;
+    let word;
+    let entry;
+
+    before(() => {
+      id = "a:1";
+      word = "a";
+      entry = createEntry(word, id, null);
+    });
+
+    it("sets the orthography", () => {
+      expect(entry.spelling).toBe(word);
+    });
+
+    it("sets the entry ID", () => {
+      expect(entry.id).toBe(id);
+    });
+  });
+
+  context("when creating an entry from raw data", () => {
     let raw;
     let entry;
     let id;
@@ -28,105 +52,61 @@ describe("Entry creator", () => {
     beforeEach(() => {
       word = "a";
       id = "a:1";
-      raw = {
-        "@n": "1",
-        "@id": "a:1",
-        "@type": "hom",
-        etym: {
-          "@orig": "a",
-          "#text": "a",
-        },
-        form: {
-          orth: "A",
-          pron: "á",
-        },
-        sense: [
-          {
-            def: "Primeira letra do alfabeto português.",
-            gramGrp: "m.",
-          },
-          {
-            "@ast": "1",
-            def: "Primeira nota da escala na denominação alfabética dos sons.",
-            usg: {
-              "@type": "dom",
-              "#text": "Mús.",
-            },
-          },
-          {
-            def: "Primeiro, (falando-se de um número ou de um objecto que faz parte de uma série).",
-            gramGrp: "Adj.",
-          },
-          {
-            "@ast": "1",
-            def: "Que é de primeira classe, (falando-se de carruagens de caminho de ferro).",
-            gramGrp: "Adj.",
-          },
-        ],
-      };
-
+      raw = defineFixture.superEntry[0].entry;
       entry = createEntry(word, id, raw);
     });
 
-    it("has an ID", () => {
+    it("sets the ID", () => {
       expect(entry.id).toBe(id);
     });
 
-    it("has a word", () => {
+    it("sets the word", () => {
       expect(entry.word).toBe(word);
     });
 
-    it("has raw content", () => {
+    it("sets the raw content", () => {
       expect(entry.raw).toEqual(raw);
     });
 
-    it("returns etymology", () => {
+    it("sets the etymology", () => {
+      raw = defineFixture.superEntry[1].entry;
+      entry = createEntry(word, id, raw);
       expect(entry.etymology).toBe(raw.etym["#text"]);
     });
 
-    it("returns orthography", () => {
+    it("sets the orthography", () => {
       expect(entry.spelling).toBe(raw.form.orth);
     });
 
-    it("returns orthography on null entry", () => {
-      entry = createEntry(word, id, null);
-      expect(entry.spelling).toBe(word);
-    });
-
-    it("returns pronunciation", () => {
+    it("sets the pronunciation", () => {
       expect(entry.pronunciation).toBe(raw.form.pron);
     });
 
-    it("returns the entry index", () => {
+    it("sets the index", () => {
       expect(entry.index).toBe(parseInt(raw["@n"], 10));
     });
 
-    it("returns the entry ID", () => {
+    it("sets the ID", () => {
       expect(entry.id).toBe(raw["@id"]);
     });
 
-    it("returns the entry ID on null entry", () => {
-      entry = createEntry(word, id, null);
-      expect(entry.id).toBe(id);
-    });
-
-    it("returns senses", () => {
+    it("sets senses", () => {
       expect(entry.senses.length).toEqual(raw.sense.length);
     });
 
-    it("returns sense definitions", () => {
+    it("sets sense definitions", () => {
       entry.senses.forEach((sense, i) => {
         expect(sense.definition).toEqual(raw.sense[i].def);
       });
     });
 
-    it("returns sense grammar groups", () => {
+    it("sets sense grammar groups", () => {
       entry.senses.forEach((sense, i) => {
         expect(sense.grammarGroup).toEqual(raw.sense[i].gramGrp);
       });
     });
 
-    it("returns sense usages", () => {
+    it("sets sense usages", () => {
       entry.senses.forEach((sense, i) => {
         expect(sense.usage).toEqual(raw.sense[i].usg && raw.sense[i].usg["#text"]);
       });
---
+++ typings/custom/deep-assign.d.ts
@@ -0,0 +1,4 @@
+declare module "deep-assign" {
+  function deepAssign(...obj: Object[]): Object;
+  export = deepAssign;
+}
