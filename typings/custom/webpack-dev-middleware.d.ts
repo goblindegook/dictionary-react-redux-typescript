971702def267607b260ac9040304d796b278a586
Luis Rodrigues <goblindegook@users.noreply.github.com>
2017-02-22 13:01:55 +0000

    Typings

--- typings/custom/webpack-dev-middleware.d.ts
+++
@@ -1,7 +0,0 @@
-declare module "webpack-dev-middleware" {
-  import express = require("express");
-  import webpack = require("webpack");
-  function webpackDevMiddleware(compiler: webpack.compiler.Compiler, options?: Object):
-    (req: express.Request, res: express.Response, next: express.NextFunction) => any;
-  export = webpackDevMiddleware;
-}
